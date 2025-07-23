47b8b8182c95b184b02f2e871106f268f6c35ff2
redev.rx <redev.rx@gmail.com>
2025-07-23 08:59:43 +0700

    complete

--- shared/src/iosMain/kotlin/com/redev/rx/rx_connection_change/ConnectivityObserver.ios.kt
+++ shared/src/iosMain/kotlin/com/redev/rx/rx_connection_change/ConnectivityObserver.ios.kt
@@ -3,6 +3,12 @@ package com.redev.rx.rx_connection_change
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import platform.Foundation.NSHTTPURLResponse
+import platform.Foundation.NSMutableURLRequest
+import platform.Foundation.NSURL
+import platform.Foundation.NSURLSession
+import platform.Foundation.dataTaskWithRequest
+import platform.Foundation.setHTTPMethod
 import platform.Network.*
 import platform.darwin.dispatch_queue_create
 import platform.darwin.dispatch_queue_t
@@ -11,7 +17,6 @@ import platform.darwin.dispatch_queue_t
 actual class ConnectivityObserver {
     private var monitor:nw_path_monitor_t? = null
     private var queue: dispatch_queue_t? = null
-    private var callback: ((Boolean) -> Unit)? = null
 
     actual val isConnected: Flow<Boolean>
         get() = callbackFlow {
@@ -25,24 +30,22 @@ actual class ConnectivityObserver {
         }
 
     private fun setupMonitor(onStatusChanged: (Boolean) -> Unit) {
-        if (monitor != null) return
-
+        stop()
         monitor = nw_path_monitor_create()
         queue = dispatch_queue_create("NetworkMonitorQueue", null)
 
-        var emitted = false
         nw_path_monitor_set_update_handler(monitor) { path ->
             val status = nw_path_get_status(path)
-            val connected = status == nw_path_status_satisfied
+            val basicConnected = status == nw_path_status_satisfied
 
-            if (!emitted) {
-                emitted = true
-                onStatusChanged(connected)
-            } else {
-                onStatusChanged(connected)
+            if(basicConnected){
+                pingInternet { hasInternet ->
+                    onStatusChanged(hasInternet && basicConnected)
+                }
+            }else {
+                onStatusChanged(basicConnected)
             }
         }
-
 
         nw_path_monitor_set_queue(monitor, queue)
         nw_path_monitor_start(monitor)
@@ -57,7 +60,22 @@ actual class ConnectivityObserver {
     }
 
     fun internetChange(callback:(Boolean)->Unit){
-        this.callback = callback
         setupMonitor(callback)
+    }
+
+    private fun pingInternet(onResult: (Boolean) -> Unit) {
+        val url = NSURL.URLWithString("https://apple.com")
+        val request = NSMutableURLRequest.requestWithURL(url!!)
+        request.setHTTPMethod("HEAD")
+
+        val session = NSURLSession.sharedSession
+
+        val task = session.dataTaskWithRequest(request) { _, response, error ->
+            val reachable = ((response as? NSHTTPURLResponse)?.statusCode ?: 500) in
+                    200..399 && error == null
+            onResult(reachable)
+        }
+
+        task.resume()
     }
 }
